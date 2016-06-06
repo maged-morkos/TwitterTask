da5397ed8c3b4c199728aeb167ae589d413be04b
maged-morkos <maged.m.morkos@gmail.com>
2016-06-07 01:25:16 +0200

    Initial Commit

---
+++ twitterTaska/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitterTaska
+//
+//  Created by Maged Morkos on 6/7/16.
+//  Copyright © 2016 Maged Morkos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
